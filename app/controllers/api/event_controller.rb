b92512254df8ecb8b5347e9396be83e11923cb42
Brajeshsen471 <brajesh.sen@jarvis.consulting>
2023-10-18 10:36:47 +0530

    add Like opertor and create api for edit event

--- app/controllers/api/event_controller.rb
+++ app/controllers/api/event_controller.rb
@@ -121,13 +121,15 @@ class Api::EventController < Api::ApplicationController
     query_conditions[:start_date] = get_date_diff(params[:start_date].to_datetime) if params[:start_date].present?
     query_conditions[:data_level] = params[:level_id] if params[:level_id].present?
     query_conditions[:status_aasm_state] = params[:event_status] if params[:event_status].present?
-    events = Event.where(query_conditions).limit(limit).offset(offset)
+    events = Event.where(query_conditions)
     events = events.joins(:event_locations).where(event_locations: {state_id: params[:state_id]}) if params[:state_id].present?
+    events = events.where("lower(name) LIKE ?", "%#{params[:search_query].downcase}%") if params[:search_query].present?
     events = events.order(created_at: :desc).limit(limit).offset(offset)
     render json: {
       data: ActiveModelSerializers::SerializableResource.new(events, each_serializer: EventSerializer, state_id: params[:state_id], current_user: current_user),
       message: ['Event list'],
-      success: true
+      success: true,
+      total: events.count
     }, status: 200
   rescue StandardError => e
     render json: { success: false, message: e.message }, status: 400
@@ -137,16 +139,15 @@ class Api::EventController < Api::ApplicationController
     limit = params[:limit].present? ? params[:limit] : 10
     offset = params[:offset].present? ? params[:offset] : 0
     query_conditions = {}
-    query_conditions[:start_date] = get_date_diff(params[:start_date].to_datetime) if params[:start_date].present?
-    query_conditions[:data_level] = params[:level_id] if params[:level_id].present?
-    query_conditions[:status_aasm_state] = params[:event_status] if params[:event_status].present?
     events = Event.where(query_conditions)
     events = events.joins(:event_locations).where(event_locations: {state_id: params[:state_id]}) if params[:state_id].present?
+    events = events.where("lower(name) LIKE ?", "%#{params[:search_query].downcase}%") if params[:search_query].present?
     events = events.order(created_at: :desc).limit(limit).offset(offset)
     render json: {
       data: ActiveModelSerializers::SerializableResource.new(events, each_serializer: EventSerializer, state_id: params[:state_id], current_user: current_user),
       message: ['Event list'],
-      success: true
+      success: true,
+      total: events.count
     }, status: 200
   rescue StandardError => e
     render json: { success: false, message: e.message }, status: 400
@@ -182,5 +183,10 @@ class Api::EventController < Api::ApplicationController
 
   def event_page
     render json: { success: true, message: "Hello World!!!" }, status: 200
+  end
+
+  def edit
+    event = Event.find_by(id: params[:event_id])
+    render json: { success: true, event: event, message: "success full" }, status: 200
   end
 end
