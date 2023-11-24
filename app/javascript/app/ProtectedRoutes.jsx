234f8732b24e6d45b9e62bc33d9864b4ecead758
amit chaudhary <chaudhary.amit@jarvis.consulting>
2023-11-24 18:14:50 +0530

    did protected routing

---
+++ app/javascript/app/ProtectedRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {Navigate, Outlet, useNavigate} from "react-router";
+
+
+const ProtectedRoutes=()=>{
+    const navigate = useNavigate();
+    const isAuthenticated  = JSON.parse(document.getElementById("app").getAttribute("data-create"));
+    if (!isAuthenticated) {
+       return <Navigate  to="/forms" replace={true} />
+    }
+    return (
+
+        <Outlet/>
+          
+    );
+}
+
+export  default ProtectedRoutes;
--- app/javascript/app/Routes.jsx
+++ app/javascript/app/Routes.jsx
@@ -9,19 +9,21 @@ import EditEvent from "./components/pages/events/edit-events/EditEvent";
 import {Navigate} from "react-router";
 
 import ViewEvents from "./components/pages/events/view-events/ViewEvents";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 const Router=()=>{
     return(
         
         <Routes >
             <Route path="/" element={<Navigate  to="/forms" replace={true} />} />
-            <Route path="/events" element={ <HomeComponent />} />
             <Route path="/forms" element={<FormComponent />} />
             <Route path="/forms/submissions/:event_id" element={<FormSubmission />} />
-
-            <Route path="/events/create" element={<CreateEvent />} />
-            <Route path="/events/edit/:id" element={<EditEvent />} />
-            <Route path="/events/view/:id"  element={<ViewEvents/>}/>
+            <Route element={<ProtectedRoutes/>}>
+                <Route path="/events" element={ <HomeComponent />} />
+                <Route path="/events/create" element={<CreateEvent />} />
+                <Route path="/events/edit/:id" element={<EditEvent />} />
+                <Route path="/events/view/:id"  element={<ViewEvents/>}/>
+            </Route>
             <Route path="/*" element={<Navigate to="/" replace={true}  />} />
         </Routes>
     )
