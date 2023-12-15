7b69c5cf78ebe64a5b7d6d892708dbbe0d2ca32c
amit chaudhary <chaudhary.amit@jarvis.consulting>
2023-12-15 18:15:34 +0530

    updated user form flow

--- app/javascript/app/components/pages/form/form-details/FormDetails.jsx
+++ app/javascript/app/components/pages/form/form-details/FormDetails.jsx
@@ -4,18 +4,22 @@ import {useParams} from "react-router-dom";
 import {toast} from "react-toastify";
 import ReactLoader from "../../../shared/loader/Loader";
 import FormEventCard from "../FormEventCard";
+import {ImageNotFound} from "../../../../assests/png";
+import './form-details.scss'
 
 
 const FormDetails=()=>{
     const {id}=useParams();
     const [childrenEvents,setChildrenEvents]=useState([]);
     const [isLoading,setIsLoading]=useState(false);
+    const [parentEvent,setParentEvent]=useState({});
     const getEventAndEventChildren=async ()=>{
         setIsLoading(true);
         try {
             const {data}=await  ApiClient.get(`event/user_children`,{params:{id}});
             if(data?.success){
                 console.log('child of event and its data',data);
+                setParentEvent(data?.data);
                 setChildrenEvents(data?.child_data);
             }else{
                 toast.error("Failed to get sub events");
@@ -30,12 +34,22 @@ const FormDetails=()=>{
     }
 
     useEffect(() => {
+        console.log('parent event is ',parentEvent);
+    }, [parentEvent]);
+
+    useEffect(() => {
                getEventAndEventChildren();
     }, []);
     return(
-        <>
+        <div className={"form-event-details-main-container"}>
           
             {isLoading?<ReactLoader/>:childrenEvents?.length===0?<div>No sub event found</div>: <div>
+                <div className="event-image-container">
+                    <img src={parentEvent?.image_url ? parentEvent?.image_url : ImageNotFound}
+                         className="event-image"/>
+
+                </div>
+
                 <div>Sub Events</div>
                 <div>
                     {childrenEvents?.map((item) => <FormEventCard event={item}/>)}
@@ -43,7 +57,7 @@ const FormDetails=()=>{
             </div>}
 
 
-        </>
+        </div>
     )
 }
 
