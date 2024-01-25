f4d2d8aaf10a8621ce1efe7b6b4f7ed543712d0d
VijayJarvis <vijay.yadav@jarvis.consulting>
2024-01-25 16:38:42 +0530

    adding title translation

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,9 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_date, :end_date, :data_level_id, :event_type, :data_level, :start_datetime, :end_datetime, :status, :states, :image_url, :state_ids, :create_form_url, :preview_url, :event_level, :has_sub_event, :parent_id
+  attributes :id, :get_event_name, :start_date, :end_date, :data_level_id, :event_type, :data_level, :start_datetime, :end_datetime, :status, :states, :image_url, :state_ids, :create_form_url, :preview_url, :event_level, :has_sub_event, :parent_id
 
+  def get_event_name
+    object&.get_title
+  end
   def data_level
     object&.data_level&.name
   end
