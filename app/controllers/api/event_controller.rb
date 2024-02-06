f1547f6e750214c78a42f5fc729d0400291e19ba
VijayJarvis <vijay.yadav@jarvis.consulting>
2024-02-06 15:12:15 +0530

    fixing edit issue

--- app/controllers/api/event_controller.rb
+++ app/controllers/api/event_controller.rb
@@ -228,10 +228,10 @@ class Api::EventController < Api::ApplicationController
   def edit
     begin
       event = Event.where(id: params[:id])
-      max_start_datetime = event.children.present? ? event.children.maximum(:start_date) : nil
-      min_end_datetime = event.children.present? ? event.children.minimum(:end_date) : nil
+      max_start_datetime = event.first.children.present? ? event.children.maximum(:start_date) : nil
+      min_end_datetime = event.first.children.present? ? event.children.minimum(:end_date) : nil
       data = ActiveModelSerializers::SerializableResource.new(event, each_serializer: EventSerializer, state_id: '', current_user: current_user)
-      render json: { success: true, data: data, start_datetime: max_start_datetime, end_datetime: min_end_datetime ,message: "successfull" }, status: :ok
+      render json: { success: true, data: data, start_datetime: max_start_datetime, end_datetime: min_end_datetime, message: "successfull" }, status: :ok
     rescue => e
       render json: { success: false, message: e.message }, status: :bad_request
     end
