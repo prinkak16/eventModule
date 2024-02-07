6245d7fca713d0536b5fd6599b04271c18660162
VijayJarvis <vijay.yadav@jarvis.consulting>
2024-02-07 22:30:15 +0530

    adding paranoid for dependent destroy

--- app/models/event_location.rb
+++ app/models/event_location.rb
@@ -1,4 +1,5 @@
 class EventLocation < ApplicationRecord
+  acts_as_paranoid
   belongs_to :event
   belongs_to :location, polymorphic: true
   belongs_to :state, foreign_key: "state_id", class_name: "Saral::Locatable::State"
