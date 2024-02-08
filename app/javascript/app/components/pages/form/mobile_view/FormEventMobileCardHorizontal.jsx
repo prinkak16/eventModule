c040f74c21ddee8fa57d186c695485fed313d277
Ashish Jajoria <akakenterprises@gmail.com>
2024-02-08 15:40:27 +0530

    handled mobile redirect for event list

--- app/javascript/app/components/pages/form/mobile_view/FormEventMobileCardHorizontal.jsx
+++ app/javascript/app/components/pages/form/mobile_view/FormEventMobileCardHorizontal.jsx
@@ -6,30 +6,30 @@ import moment from "moment";
 import EllipsesComponent from "../../../../utils/EllipsesComponent";
 import {useNavigate} from "react-router";
 
-const FormEventMobileCardHorizontal=({event,index})=>{
+const FormEventMobileCardHorizontal = ({event, index}) => {
 
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
     const submissionHandler = (event_id) => {
         navigate(`/forms/${event_id}`);
     };
 
-    return(
-        <div className="form-event-container-horizontal" onClick={()=>submissionHandler(event?.id)}>
-            <div className="bg-img-container">
-                <img src={event?.image_url ? event?.image_url : DefaultImage} alt="loading..." className="bg-img"/>
-                <div className="event-details-container">
-                    <EllipsesComponent text={event?.name}/>
-                    <div className="flex-details">
+    return (
+            <div className="form-event-container-horizontal" onClick={()=>submissionHandler(event?.id)}>
+                <div className="bg-img-container">
+                    <img src={event?.image_url ? event?.image_url : DefaultImage} alt="loading..." className="bg-img"/>
+                    <div className="event-details-container">
+                        <EllipsesComponent text={event?.name}/>
+                        <div className="flex-details">
                             <EllipsesComponent text={moment(event?.start_date).format("DD MMM YY")}
                             />
                             <EllipsesComponent
                                 text={moment(event?.start_datetime, "YY-MM-DD hh:mm:ss A").format('h:mm a')}
                             />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
