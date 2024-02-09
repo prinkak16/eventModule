38022ed8c39045aba8b75f6e990f7f96bad0f550
navinJarvis <naveen.chand@jarvis.consulting>
2024-02-09 14:14:46 +0530

    added shimmer effect for horizontal cards events for mobile view

---
+++ app/javascript/app/components/pages/shimmer_effects/HorizontalCardShimmerEffect.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {ShimmerPostList} from "react-shimmer-effects";
+const HorizontalCardShimmerEffect = ()=>{
+    return(
+      <ShimmerPostList postStyle="STYLE_FOUR" col={3} row={1} gap={30} />
+    )
+}
+export default HorizontalCardShimmerEffect;
