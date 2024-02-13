b3109740a381b02c41bc1c987563aa905e925390
VijayJarvis <vijay.yadav@jarvis.consulting>
2024-02-13 18:44:10 +0530

    running mongo query batch wise

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -2,7 +2,7 @@ class Api::ReportsController < ApplicationController
 
   def fetch_reports
     begin
-      FetchReportsJob.perform_async(params[:event_id], params[:email_id].split(','))
+      Resque.enqueue(FetchReportsJob, params[:event_id], params[:email_id].split(','))
       render json: { success: true, message: "the report will be send to #{params[:email_id]}." }, status: :ok
     rescue => e
       render json: { success: false, message: e.message }, status: :bad_request
