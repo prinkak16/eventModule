51bf23ba715ffe877918e99501fa78bc3fe77613
amit chaudhary <chaudhary.amit@jarvis.consulting>
2024-02-14 17:15:29 +0530

    added report-time-line in report modal

--- app/javascript/app/components/shared/constants/index.jsx
+++ app/javascript/app/components/shared/constants/index.jsx
@@ -1,1 +1,1 @@
-export const reportTimeline=["0.5hr","1hr","2hr","3hr","4hr","5hr","6hr","7hr","8hr","9hr","10hr","12hr","24hr"]
+export const reportTimeline=["0.5hr","1hr","2hr","3hr","4hr","5hr","6hr","7hr","8hr","9hr","10hr","12hr","24hr","Overall"]
