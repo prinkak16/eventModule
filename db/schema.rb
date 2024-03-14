e52edb19306c0ece5cf98bdf29b4ad0ff63e5138
VIJAY YADAV <vijayyadav@VIJAYs-MacBook-Pro.local>
2024-03-14 15:37:37 +0530

    adding csv tables in event module

--- db/schema.rb
+++ db/schema.rb
@@ -67,20 +67,22 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_125100) do
   create_table "event_submissions", force: :cascade do |t|
     t.string "form_id"
     t.string "submission_id"
+    t.bigint "user_id", null: false
     t.bigint "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.datetime "deleted_at"
     t.index ["created_at"], name: "index_event_submissions_on_created_at", order: :desc
     t.index ["deleted_at"], name: "index_event_submissions_on_deleted_at"
     t.index ["event_id", "user_id", "submission_id", "form_id"], name: "index_event_submissions_on_event_user_submission_form"
     t.index ["event_id"], name: "index_event_submissions_on_event_id"
+    t.index ["user_id"], name: "index_event_submissions_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
+    t.bigint "data_level_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status_aasm_state", default: "upcoming"
@@ -88,6 +90,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_125100) do
     t.integer "state_id"
     t.datetime "start_date"
     t.datetime "end_date"
+    t.bigint "created_by_id", null: false
     t.datetime "deleted_at"
     t.boolean "published", default: false
     t.integer "parent_id"
@@ -95,9 +98,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_125100) do
     t.jsonb "translated_title"
     t.integer "position"
     t.boolean "pinned", default: false
-    t.integer "created_by_id"
-    t.integer "data_level_id"
     t.boolean "is_hidden", default: false
+    t.index ["created_by_id"], name: "index_events_on_created_by_id"
+    t.index ["data_level_id"], name: "index_events_on_data_level_id"
     t.index ["deleted_at"], name: "index_events_on_deleted_at"
     t.index ["translated_title"], name: "index_events_on_translated_title", using: :gin
   end
@@ -119,31 +122,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_125100) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_event_locations", force: :cascade do |t|
-    t.string "location_type"
-    t.bigint "location_id"
-    t.bigint "user_event_id"
-    t.integer "country_state_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["location_type", "location_id"], name: "index_user_event_locations_on_location"
-    t.index ["user_event_id"], name: "index_user_event_locations_on_user_event_id"
-  end
-
-  create_table "user_events", force: :cascade do |t|
-    t.string "phone_number"
-    t.boolean "disabled", default: false
-    t.bigint "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_user_events_on_event_id"
-    t.index ["phone_number"], name: "index_user_events_on_phone_number"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "event_locations", "events"
   add_foreign_key "event_submissions", "events"
-  add_foreign_key "user_event_locations", "user_events"
-  add_foreign_key "user_events", "events"
 end
