5707220c6f7fd8394f3480428892ef5b2f03d9c8
amit chaudhary <chaudhary.amit@jarvis.consulting>
2024-03-15 11:51:15 +0530

    updated hide-unhide-events

--- app/javascript/app/components/pages/events/event-details/EventDetails.jsx
+++ app/javascript/app/components/pages/events/event-details/EventDetails.jsx
@@ -12,11 +12,10 @@ import DraggableList from "./drag-and-drop-components/DraggableList";
 import {hideUnhideEvents} from "../../../../services/CommonServices/commonServices";
 import CircularProgress from "@mui/material/CircularProgress";
 import ReactLoader from "../../../shared/loader/Loader";
+import ConfirmationModal from "../../../shared/ConfirmationModal/ConfirmationModal";
 
 
 const EventDetails = () => {
-    const [parent, setParent] = useState(null);
-
     const {pathname} = useLocation();
     const navigate = useNavigate();
     const [loader,setLoader]=useState(false);
@@ -26,9 +25,29 @@ const EventDetails = () => {
     const [reportEventId, setReportEventId] = useState("");
     const [reportModal, setReportModal] = useState(false);
     const [hideButtonLoader,setHideButtonLoader]=useState(false);
-
-
+    //states relate to hide-unhide-modal
+    const [hideUnhideData, setHideUnhideData] = useState({
+        title: "",
+        message: "",
+        body: "",
+        confirmationButtonText: "",
+        note: "",
+        event_id: "",
+        is_hidden: ""
+    });
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+    const [confirmationStatus, setConfirmationStatus] = useState(false);
     const {id} = useParams();
+
+    useEffect(() => {
+        if (confirmationStatus) {
+            hideAndUnhideEvent();
+        }
+    }, [confirmationStatus]);
+
+    useEffect(() => {
+        getEventDetails();
+    }, [pathname]);
 
     const getEventDetails = async () => {
         setLoader(true);
@@ -87,7 +106,8 @@ const updateChildrenEventsPosition=async (body)=>{
         toast.error('Failed during position update')
     }
 }
-const hideAndUnhideEvents=async (body)=>{
+const hideAndUnhideEvent=async ()=>{
+    const body= {is_hidden:!hideUnhideData?.is_hidden, event_id:hideUnhideData?.event_id}
     setHideButtonLoader(true)
     try{
         const {data}=await hideUnhideEvents(body);
@@ -101,13 +121,9 @@ const hideAndUnhideEvents=async (body)=>{
       toast.error(e?.message);
     }finally {
         setHideButtonLoader(false)
+        setConfirmationStatus(false)
     }
 }
-
-    useEffect(() => {
-        getEventDetails();
-    }, [pathname]);
-
     const handleClick = (event_action) => {
         if (event_action === 'create_event') {
             navigate(`/events/create/${id}`);
@@ -116,10 +132,31 @@ const hideAndUnhideEvents=async (body)=>{
         }
     }
 
+    const enableHideConfirmationModal = (body) => {
+        const title = body?.is_hidden ? "Unhide Event" : "Hide Event";
+        const message = body?.is_hidden ? "Are you sure you want to unhide the event ?" : "Are you sure you want to hide the event";
+        const confirmationButtonText = body?.is_hidden ? "Unhide" : "Hide";
+        const note = body?.is_hidden ? "Unhiding this event will show its sub-events to users" : "Hiding this event will hide its sub-events from users"
+        setHideUnhideData((prevData) => ({
+            ...prevData,
+            message: message,
+            title: title,
+            confirmationButtonText: confirmationButtonText,
+            event_id: body?.event_id,
+            is_hidden: body?.is_hidden,
+            note: note
+        }))
+        setShowConfirmationModal(true)
+    }
 
 
     return (
         <div className={"event-details-main-container"}>
+            <ConfirmationModal title={hideUnhideData?.title} message={hideUnhideData?.message} note={hideUnhideData?.note}
+                               showConfirmationModal={showConfirmationModal}
+                               setShowConfirmationModal={setShowConfirmationModal}
+                               setConfirmationStatus={setConfirmationStatus}
+                               confirmationButtonText={hideUnhideData?.confirmationButtonText}/>
             {loader?<ReactLoader/>:<>
             <ReportEmailModal reportModal={reportModal} setReportModal={setReportModal} reportEventId={reportEventId}/>
             <div className={"heading"}>Event Details</div>
@@ -132,9 +169,9 @@ const hideAndUnhideEvents=async (body)=>{
                         <Button disabled={hideButtonLoader} onClick={() => {
                             const body = {
                                 event_id: parentEvent?.id,
-                                is_hidden: !parentEvent?.is_hidden
+                                is_hidden: parentEvent?.is_hidden
                             }
-                            hideAndUnhideEvents(body)
+                            enableHideConfirmationModal(body)
                         }} className={`add-event-button ${hideButtonLoader&&'loader-button-style'}`}
                                 variant={"contained"}>{hideButtonLoader?<CircularProgress className={"loader-style"}/>: (parentEvent?.is_hidden?"UnHide event":"Hide event")}</Button>
 
@@ -160,20 +197,21 @@ const hideAndUnhideEvents=async (body)=>{
                             <Button onClick={() => handleClick('go_to_form')} className={"add-event-button"}
                                 variant={"contained"}>Go to form</Button>
                         }
-                        <Button disabled={hideButtonLoader} onClick={() => {
-                            const body = {
-                                event_id: parentEvent?.id,
-                                is_hidden: !parentEvent?.is_hidden
-                            }
-                            hideAndUnhideEvents(body)
-                        }} className={`add-event-button ${hideButtonLoader&&'loader-button-style'}`}
-                                variant={"contained"}> {hideButtonLoader?<CircularProgress className={"loader-style"}/>: (parentEvent?.is_hidden?"UnHide event":"Hide event")} </Button>
 
                         <Button onClick={() => {
                             setReportEventId(id)
                             setReportModal(true)
                         }} className={"add-event-button"}
                                 variant={"contained"}>Download Report</Button>
+
+                        <Button disabled={hideButtonLoader} onClick={() => {
+                            const body = {
+                                event_id: parentEvent?.id,
+                                is_hidden: parentEvent?.is_hidden
+                            }
+                            enableHideConfirmationModal(body)
+                        }} className={`add-event-button ${hideButtonLoader&&'loader-button-style'}`}
+                                variant={"contained"}> {hideButtonLoader?<CircularProgress className={"loader-style"}/>: (parentEvent?.is_hidden?"UnHide event":"Hide event")} </Button>
                     </div>
 
                 <div className={"heading"}>Form View</div>
