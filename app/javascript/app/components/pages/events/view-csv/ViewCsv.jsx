f80c24bf297d738044a6a1ca8129ea904987265a
amit chaudhary <chaudhary.amit@jarvis.consulting>
2024-03-21 14:59:03 +0530

    updated csv-upload

--- app/javascript/app/components/pages/events/view-csv/ViewCsv.jsx
+++ app/javascript/app/components/pages/events/view-csv/ViewCsv.jsx
@@ -11,11 +11,6 @@ import {
 import {toast} from "react-toastify";
 import {useParams} from "react-router";
 import moment from "moment";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from "@mui/material/FormControl";
-
 const eventLevels = [{"pradesh": 1}, {"vibhag": 1}, {"Lok Sabha": 1}, {"Zila": 1}, {"Vidhan Sabha": 1}, {"Mandal": 1}, {"Shakti Kendara": 1}, {"Booth": 2}, {"Total count": 100}];
 const tableHeader = ["S. No.", "Phone No.", "State", "Location Type", "Location Name/No.", "Location Filter"]
 const tableBody = [[1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134-asdf f Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"]]
@@ -23,7 +18,13 @@ const ViewCsv = () => {
     const {id} = useParams();
     const rowPerPage = 10;
     const [page, setPage] = useState(1);
-    const [EventLevels, setEventLevels] = useState(eventLevels);
+    const [csvDetails, setCsvDetails] = useState({
+        locationData:{},
+        tableHeader:[],
+        tableContent:[],
+        totalCount:null,
+
+    });
     const [recentlyUploadedFiles,setRecentlyUploadedFiles]=useState([]);
     const [searchQuery, setSearchQuery] = useState(null);
     const {setShowCsvModal} = EventState();
@@ -52,7 +53,12 @@ const ViewCsv = () => {
         const queryParams = {event_id: id,searchQuery, offset: (page - 1) * rowPerPage, limit: rowPerPage}
         try {
             const {data} = await getEventCsvData(queryParams);
-            console.log('data ', data);
+            if(data?.success){
+                setCsvDetails(prevState => ({...prevState,locationData: data?.location_data,tableHeader:data?.headers,tableContent: data?.data,totalCount:data?.count??data?.data?.length}))
+            }
+            else{
+                toast.error(data?.message);
+            }
         } catch (e) {
             toast.error(e?.message)
         }
@@ -93,17 +99,17 @@ const ViewCsv = () => {
                     <label htmlFor="recent-files">Show</label>
                     <select name="recent-files" id="recent-files" className={"recent-files-dropdown-container"}>
                         <option disabled selected value="default">Recent Files</option>
-                        {recentlyUploadedFiles?.map((item) => <option value={`${item?.file_name} ${item?.date}`}>{`${item?.file_name} ${moment(item?.date).format("DD MMM YYYY, hh:mm A")}`}</option>
+                        {recentlyUploadedFiles?.map((item) => <option value={`${item?.file_name} ${item?.date}`}>{`${item?.file_name} ${item?.date}`}</option>
                         )}
                     </select>
                 </div>
                 <button className={"csv-button"} onClick={() => setShowCsvModal(true)}>Add / Delete Reportees</button>
             </div>
             <div className={"level-filer-container"}>
-                {EventLevels?.map((level) => Object.entries(level).map(([key, value]) => <Chip
-                    label={`${key} ${value}`}
+                {Object.keys(csvDetails?.locationData)?.map(key=><Chip
+                    label={`${key} ${csvDetails?.locationData[key]}`}
                     className={`level-filter-chip ${key.toLowerCase()==='total count'?'highlighted-chip':''}`}
-                />))
+                />)
                 }
             </div>
             <div className={'search-bar-container'}>
@@ -114,14 +120,13 @@ const ViewCsv = () => {
             <div className={"table-container"}>
                 <table>
                     <tr className={`table-header`}>
-                        {tableHeader?.map((header) => <th>{header}</th>)}
+                        {csvDetails?.tableHeader?.map((header) => <th>{header}</th>)}
                     </tr>
-                    {tableBody?.map((tablerow) => <tr>
-                        {tablerow?.map(data => <td>{data}</td>)}
+                    {csvDetails?.tableContent?.map((tableRow) => <tr>
+                        {tableRow?.map(data => <td>{data}</td>)}
                     </tr>)}
-
                 </table>
-                <Pagination count={10} variant="outlined" shape="rounded" className={"pagination-item"} colSpan={3}
+                <Pagination count={Math.ceil(csvDetails?.totalCount/rowPerPage)} variant="outlined" shape="rounded" className={"pagination-item"} colSpan={3}
                             page={page}
                             onChange={handlePageChange}
                 />
