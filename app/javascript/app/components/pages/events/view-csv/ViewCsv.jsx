315548cd66832911b62d68237e2acc237717d6da
amit chaudhary <chaudhary.amit@jarvis.consulting>
2024-03-21 15:04:07 +0530

    updated csv-upload

--- app/javascript/app/components/pages/events/view-csv/ViewCsv.jsx
+++ app/javascript/app/components/pages/events/view-csv/ViewCsv.jsx
@@ -10,10 +10,6 @@ import {
 } from "../../../../services/CommonServices/commonServices";
 import {toast} from "react-toastify";
 import {useParams} from "react-router";
-import moment from "moment";
-const eventLevels = [{"pradesh": 1}, {"vibhag": 1}, {"Lok Sabha": 1}, {"Zila": 1}, {"Vidhan Sabha": 1}, {"Mandal": 1}, {"Shakti Kendara": 1}, {"Booth": 2}, {"Total count": 100}];
-const tableHeader = ["S. No.", "Phone No.", "State", "Location Type", "Location Name/No.", "Location Filter"]
-const tableBody = [[1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134-asdf f Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"], [1, 9876543210, "Rajsthan", "Booth", "2134- Booth ABC", "zila"]]
 const ViewCsv = () => {
     const {id} = useParams();
     const rowPerPage = 10;
@@ -24,7 +20,7 @@ const ViewCsv = () => {
         tableContent:[],
         totalCount:null,
 
-    });
+    })
     const [recentlyUploadedFiles,setRecentlyUploadedFiles]=useState([]);
     const [searchQuery, setSearchQuery] = useState(null);
     const {setShowCsvModal} = EventState();
