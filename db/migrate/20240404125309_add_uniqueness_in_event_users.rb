cfedca8c4b3da84a73565a1e23fb5ce696be935d
VIJAY YADAV <vijayyadav@VIJAYs-MacBook-Pro.local>
2024-04-04 18:37:07 +0530

    changing migration

--- db/migrate/20240404125309_add_uniqueness_in_event_users.rb
+++ db/migrate/20240404125309_add_uniqueness_in_event_users.rb
@@ -1,5 +1,6 @@
 class AddUniquenessInEventUsers < ActiveRecord::Migration[7.0]
   def change
+    remove_index :event_users, [ :event_id, :phone_number ]
     add_index :event_users, [:event_id, :phone_number], unique: true
     add_index :event_user_locations, [ :event_user_id, :phone_number, :country_state_id, :location_type, :location_id ], name: 'index_event_user_locations_on_epcl', unique: true
   end
